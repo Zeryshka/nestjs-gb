935744eeb35dcd6229845f05b77fe12b1238b2e0
Zeryshka <maxim98sorokin@gmail.com>
2022-03-29 00:09:35 +0300

    Lesson #1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { News } from './dto/news.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Query,
+  HttpException,
+} from '@nestjs/common';
+import { AppService, AppCalculator } from './app.service';
 
-@Controller()
+@Controller('news')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get')
+  async get(@Query('id') id: number): Promise<HttpException | News> {
+    return this.appService.get(id);
+  }
+
+  @Get('getAll')
+  async getAll(): Promise<News[]> {
+    return this.appService.getAll();
+  }
+
+  @Put('create')
+  async createNews(@Body() body: News): Promise<News[]> {
+    return this.appService.createNews(body);
+  }
+
+  @Post('update')
+  async updateNews(
+    @Query() queary: { id: number },
+    @Body() body: News,
+  ): Promise<News[]> {
+    return this.appService.updateNews(queary.id, body);
+  }
+
+  @Delete('delete')
+  async deleteNews(@Query() queary: { id: number }): Promise<News[]> {
+    return this.appService.deleteNews(queary.id);
+  }
+}
+
+// localhost:3000/calculator/plus
+// body: [1,2,3]
+// response: 6
+@Controller('calculator')
+export class CalculatorController {
+  constructor(private readonly appCalculator: AppCalculator) {}
+
+  @Post('plus')
+  async plus(@Body() body: number[]): Promise<number> {
+    return this.appCalculator.plus(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { News } from './dto/news.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
+const news: News[] = [
+  {
+    id: 0,
+    title: 'Котики',
+    description:
+      'В мире котиков существует один самый красивый котик и его зовут Вася...',
+    author: 'Котик Вася',
+    createdAt: new Date(Date.now()),
+  },
+];
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async get(id: number): Promise<News | HttpException> {
+    if (news[id]) {
+      return news[id];
+    }
+
+    throw new HttpException(
+      {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: `Запись с id ${id} не найдена`,
+      },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+
+  async getAll(): Promise<News[]> {
+    return news;
+  }
+
+  async createNews(data: News): Promise<News[]> {
+    data.id = news.length;
+    data.createdAt = new Date(Date.now());
+    news.push(data);
+    return news;
+  }
+
+  async updateNews(id: number, data: News): Promise<News[]> {
+    if (news[id]) {
+      news[id] = {
+        ...news[id],
+        ...data,
+      };
+      return news;
+    }
+
+    throw new HttpException(
+      {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: `Запись с id ${id} не найдена`,
+      },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+
+  async deleteNews(id: number): Promise<News[]> {
+    news.splice(id, 1);
+    return news;
+  }
+}
+
+@Injectable()
+export class AppCalculator {
+  async plus(numbers: number[]): Promise<number> {
+    return numbers.reduce(function (sum, elem) {
+      return sum + elem;
+    }, 0);
   }
 }
