df73776cdc3e6e1ae700bd9730bafe160426f9f2
Zeryshka <maxim98sorokin@gmail.com>
2022-04-10 22:57:33 +0300

    Done lesson #5

---
+++ src/api/api.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PostsModule } from './modules/posts/posts.module';
+import { CommentsModule } from './modules/comments/comments.module';
+import { PostsController } from './controllers/posts.controller';
+import { CommentsController } from './controllers/comments.controller';
+import { MailModule } from '../mail/mail.module';
+import { MailController } from '../mail/mail.controller';
+
+@Module({
+  imports: [PostsModule, CommentsModule, MailModule],
+  controllers: [PostsController, CommentsController, MailController],
+})
+export class ApiModule {}
